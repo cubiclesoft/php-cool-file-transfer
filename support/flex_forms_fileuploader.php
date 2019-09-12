d072b7f7ddab2a170ebf6c6d9621d5647120b5cd
cubiclesoft <webmaster@cubiclesoft.com>
2019-09-12 05:58:04 -0700

    Latest library.

--- support/flex_forms_fileuploader.php
+++ support/flex_forms_fileuploader.php
@@ -138,10 +138,10 @@ jQuery(function() {
 
 		public static function GetFileStartPosition()
 		{
-			if (isset($_SERVER["HTTP_CONTENT_RANGE"]))
+			if (isset($_SERVER["HTTP_CONTENT_RANGE"]) || isset($_SERVER["HTTP_RANGE"]))
 			{
 				// Content-Range: bytes (*|integer-integer)/(*|integer-integer)
-				$vals = explode(" ", preg_replace('/\s+/', " ", str_replace(",", "", $_SERVER["HTTP_CONTENT_RANGE"])));
+				$vals = explode(" ", preg_replace('/\s+/', " ", str_replace(",", "", (isset($_SERVER["HTTP_CONTENT_RANGE"]) ? $_SERVER["HTTP_CONTENT_RANGE"] : $_SERVER["HTTP_RANGE"]))));
 				if (count($vals) === 2 && strtolower($vals[0]) === "bytes")
 				{
 					$vals = explode("/", trim($vals[1]));
